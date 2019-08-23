a3fe31e182936b8aef05efca4c317236e3f157bb
Kildare Lauser <kildarealauser@gmail.com>
2019-08-23 19:51:30 -0400

    Updates

---
+++ precache-manifest.38ffb35ed994cff18aec13c51692c287.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b95324234c2445d0726bd26dd27fe1",
+    "url": "/pokedex-reactjs/index.html"
+  },
+  {
+    "revision": "0200c74064659900c979",
+    "url": "/pokedex-reactjs/static/css/main.b5fcfecd.chunk.css"
+  },
+  {
+    "revision": "0294a1f8767e96d549ad",
+    "url": "/pokedex-reactjs/static/js/2.e2c79118.chunk.js"
+  },
+  {
+    "revision": "0200c74064659900c979",
+    "url": "/pokedex-reactjs/static/js/main.0fcc8fc8.chunk.js"
+  },
+  {
+    "revision": "6df3abcc2efa998fb95a",
+    "url": "/pokedex-reactjs/static/js/runtime~main.f958c5e0.js"
+  }
+]);
